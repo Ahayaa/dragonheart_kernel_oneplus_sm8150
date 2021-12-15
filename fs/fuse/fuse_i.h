a32edc2475d9e63384acaf2932bf9da5497d4bbf
Cyber Knight <cyberknight755@gmail.com>
2021-12-15 13:22:48 +0800

    fuse: Import `fuse_shortcircuit` from OxygenOS 11.0.0.2 drop
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -155,6 +155,9 @@ struct fuse_file {
 
 	/** Has flock been performed on this file? */
 	bool flock:1;
+  
+  	/* the read write file */
+	struct file *rw_lower_file;
 };
 
 /** One input argument of a request */
@@ -235,6 +238,9 @@ struct fuse_args {
 		unsigned numargs;
 		struct fuse_arg args[2];
 	} out;
+        /** fuse shortcircuit file  */
+        struct file *private_lower_rw_file;
+        char *iname;
 };
 
 #define FUSE_ARGS(args) struct fuse_args args = {}
@@ -384,6 +390,10 @@ struct fuse_req {
 
 	/** Request is stolen from fuse_file->reserved_req */
 	struct file *stolen_file;
+
+        /** fuse shortcircuit file */
+        struct file *private_lower_rw_file;
+        char *iname;
 };
 
 struct fuse_iqueue {
@@ -543,6 +553,9 @@ struct fuse_conn {
 
 	/** handle fs handles killing suid/sgid/cap on write/chown/trunc */
 	unsigned handle_killpriv:1;
+
+        /** Shortcircuited IO. */
+        unsigned shortcircuit_io:1;
 
 	/*
 	 * The following bitfields are only for optimization purposes
@@ -985,5 +998,6 @@ extern const struct xattr_handler *fuse_acl_xattr_handlers[];
 struct posix_acl;
 struct posix_acl *fuse_get_acl(struct inode *inode, int type);
 int fuse_set_acl(struct inode *inode, struct posix_acl *acl, int type);
+extern int sct_mode;
 
 #endif /* _FS_FUSE_I_H */
